e8ec1d2d71dde2a3d01ace59596fe76956aed3e0
Martynas <packmaster@riawolf.com>
2018-10-17 20:49:27 +0300

    added animal class

--- src/lt/kaunascoding/model/vo/Cat.java
+++ src/lt/kaunascoding/model/vo/Cat.java
@@ -2,66 +2,16 @@ package lt.kaunascoding.model.vo;
 
 import lt.kaunascoding.model.strategies.IActionStrategy;
 
-public class Cat implements IAnimal {
-    private String name;
-    private String type;
-    private float weight;
-    private float height;
-    private IActionStrategy strategy;
+public class Cat extends Animal implements IAnimal {
 
-    public Cat(IActionStrategy parStrategy){
-        strategy = parStrategy;
+    public Cat(IActionStrategy parStrategy) {
+
+        setStrategy(parStrategy);
     }
 
-    @Override
-    public String getType() {
-        return type;
-    }
-
-    @Override
-    public void setType(String parType) {
-        type = parType;
-
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public void setName(String parName) {
-        name = parName;
-
-    }
-
-    @Override
-    public float getWeight() {
-        return weight;
-    }
-
-    @Override
-    public void setWeight(float parWeight) {
-        weight = parWeight;
-    }
-
-    @Override
-    public float getHeight() {
-        return height;
-    }
-
-    @Override
-    public void setHeight(float parHeight) {
-        height = parHeight;
-    }
-
-    @Override
-    public void doAction() {
-        strategy.doAction();
-    }
     @Override
     public String toString() {
         doAction();
-        return "Im a cat: "+ name+ " " + type +" "+ weight+ " "+ height;
+        return "Im a cat: " + super.toString();
     }
 }
--- src/lt/kaunascoding/model/vo/Dog.java
+++ src/lt/kaunascoding/model/vo/Dog.java
@@ -2,67 +2,15 @@ package lt.kaunascoding.model.vo;
 
 import lt.kaunascoding.model.strategies.IActionStrategy;
 
-public class Dog implements IAnimal {
-    private String name;
-    private String type;
-    private float weight;
-    private float height;
-    private IActionStrategy strategy;
-
+public class Dog extends Animal implements IAnimal {
     public Dog(IActionStrategy parStrategy) {
-        strategy = parStrategy;
-    }
 
-    @Override
-    public String getType() {
-        return type;
-    }
-
-    @Override
-    public void setType(String parType) {
-        type = parType;
-
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public void setName(String parName) {
-        name = parName;
-
-    }
-
-    @Override
-    public float getWeight() {
-        return weight;
-    }
-
-    @Override
-    public void setWeight(float parWeight) {
-        weight = parWeight;
-    }
-
-    @Override
-    public float getHeight() {
-        return height;
-    }
-
-    @Override
-    public void setHeight(float parHeight) {
-        height = parHeight;
-    }
-
-    @Override
-    public void doAction() {
-        strategy.doAction();
+        setStrategy(parStrategy);
     }
 
     @Override
     public String toString() {
         doAction();
-        return "Im a dog: "+ name+ " " + type +" "+ weight+ " "+ height;
+        return "Im a dog: " + super.toString();
     }
 }

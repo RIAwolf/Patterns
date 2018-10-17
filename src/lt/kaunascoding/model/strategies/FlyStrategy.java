4782d340c793b86fe054ee90d8e775f4c17e8237
Martynas <packmaster@riawolf.com>
2018-10-17 20:30:36 +0300

    all features

---
+++ src/lt/kaunascoding/model/strategies/FlyStrategy.java
@@ -0,0 +1,10 @@
+package lt.kaunascoding.model.strategies;
+
+public class FlyStrategy implements IActionStrategy {
+
+
+    @Override
+    public void doAction() {
+        System.out.println("I Fly");
+    }
+}
